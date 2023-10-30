6b730a11d2aff0b0ba0b83ee2f021581fe85cd62
wojtas558 <92813453+wojtas558@users.noreply.github.com>
2023-10-31 00:01:20 +0100

    Added js functions that allow  to preview featured screenshot  -Added <a> tag to all featured slides  -Added animations to categories  -Changed indentation  -Added comments  -Changed content in one of featured slides

--- furni-1.0.0/js/js.js
+++ furni-1.0.0/js/js.js
@@ -3,3 +3,13 @@ function calcView()
     height = String(document.getElementById("title").clientHeight * -1 / 2);
     document.getElementById("title").style.marginTop = String(height) + "px";
 }
+
+
+function showScreenshot(slide, screenshot)
+{
+    document.getElementById("s" + String(slide) + "capsule").src = "img/featured/game" + String(slide) + "/screenshot" + String(screenshot) + ".jpg";
+}
+function changeCapsuleBack(slide)
+{
+    document.getElementById("s" + String(slide) + "capsule").src = "img/featured/game" + String(slide) + "/capsule.jpg";
+}
